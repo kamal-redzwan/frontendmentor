24b8c862803cc22dfb770cef76bc6bec517aacd4
Kamal Redzwan <109417261+kamal-redzwan@users.noreply.github.com>
2024-12-26 19:20:10 +0800

    20 - implement form validation + hover styles for button and social icons

--- 20-ping-coming-soon-page/script.js
+++ 20-ping-coming-soon-page/script.js
@@ -1,6 +1,8 @@
-const inputEmail = document.querySelector('.input_email');
-const errorLog = document.querySelector('.error_log');
-if (!inputEmail.checkValidity()) {
-  errorLog.style.display = 'block';
-  inputEmail.style.border = '2px solid var(--light-red)';
+function validateForm() {
+  const inputEmail = document.querySelector('.input_email');
+  const errorLog = document.querySelector('.error_log');
+  if (!inputEmail.checkValidity()) {
+    errorLog.style.display = 'block';
+    inputEmail.style.border = '2px solid var(--light-red)';
+  }
 }
